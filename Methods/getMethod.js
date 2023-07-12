494dbad322a22b336cf0fa41cee0a46ecdad5746
detectiveseo <detectiveseo1@gmail.com>
2023-07-12 18:08:23 +0600

    single room dataget

--- Methods/getMethod.js
+++ Methods/getMethod.js
@@ -1,6 +1,16 @@
+const { ObjectId } = require("mongodb");
+
 module.exports = getMethod = (app, usersCollection, roomsCollection) => {
     app.get("/all-rooms", async (req, res) => {
         const result = await roomsCollection.find().toArray();
+        res.send(result);
+    })
+
+    //get single room data 
+    app.get("/room/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await roomsCollection.find(query).toArray();
         res.send(result);
     })
 }
